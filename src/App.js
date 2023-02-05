a0da21ba6c882a0d64c902e754bf43055426b7b8
DevinLlerena23 <devinllerena@gmail.com>
2023-02-05 16:11:49 -0500

    calculadora

--- src/App.js
+++ src/App.js
@@ -2,26 +2,42 @@
 import { useState } from 'react';
 import './App.css';
 import Boton from './Components/Boton';
+import { evaluate } from 'mathjs';
 
 function App() {
   const [valor,setValor]=useState("")
-
+     const handleKeyPress = (event) => {
+       const key = event.key;
+       if (key >= "0" && key <= "9") {
+         hacerClic(key);
+       } else if (key === "+" || key === "-" || key === "*" || key === "/") {
+         hacerClic(key);
+       } else if (key === "Enter") {
+         calcularClic();
+       } else if (key === "Escape" || key === "c" || key === "C") {
+         setValor("");
+       }
+     };
+  
+  
   const hacerClic=(texto)=>{
     //console.log(texto)
     setValor(valor+texto)
   }
   const calcularClic=()=>{
-    console.log("La operacion a calcular es",valor)
+    let resultado = evaluate(valor);
+    setValor (resultado)
+    console.log("La operacion a calcular es", valor)
   }
   return (
     <div className="App">
       Calculadora
       <div>
-        <input type="text" value={valor} readOnly />
+        <input type="text" value={valor} onKeyPress={handleKeyPress} />
       </div>
       <div>
-        <Boton hacerClic={hacerClic}>1</Boton>
-        <Boton hacerClic={hacerClic}>2</Boton>
+        <Boton  hacerClic={hacerClic}>1</Boton>
+        <Boton  hacerClic={hacerClic}>2</Boton>
         <Boton hacerClic={hacerClic}>3</Boton>
         <Boton hacerClic={hacerClic}>+</Boton>
       </div>
@@ -32,7 +48,7 @@ function App() {
         <Boton hacerClic={hacerClic}>-</Boton>
       </div>
       <div>
-        <Boton hacerClic={hacerClic}>7</Boton>
+        <Boton className='boton' hacerClic={hacerClic}>7</Boton>
         <Boton hacerClic={hacerClic}>8</Boton>
         <Boton hacerClic={hacerClic}>9</Boton>
         <Boton hacerClic={hacerClic}>*</Boton>
@@ -44,8 +60,8 @@ function App() {
         <Boton hacerClic={hacerClic}>/</Boton>
       </div>
       <div>
-        <Boton hacerClic={()=>setValor("")}>clear</Boton>
-        </div>
+        <Boton hacerClic={() => setValor("")}>clear</Boton>
+      </div>
     </div>
   );
 }
